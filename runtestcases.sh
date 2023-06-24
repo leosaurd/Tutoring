17f02aee0d79b2d9e167b4ff2f0dc6d71891d8d0
leosaurd <deathsticks@hotmail.com>
2023-06-25 06:30:17 +1200

    Sample shell script to automate test cases.

---
+++ runtestcases.sh
@@ -0,0 +1,7 @@
+cd /home/srchi95/Sri_Assignment2
+./ppd stock.dat coins.dat < test1.input > test1.actual_ppd_out
+./ppd stock.dat coins.dat < test2.input > test2.actual_ppd_out
+./ppd stock.dat coins.dat < test3.input > test3.actual_ppd_out
+./ppd stock.dat coins.dat < test4.input > test4.actual_ppd_out
+./ppd stock.dat coins.dat < test5.input > test5.actual_ppd_out
+./ppd stock.dat coins.dat < test6.input > test6.actual_ppd_out
